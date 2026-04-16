85e19334d27c92935a75ab7c45661696154e6265
divyagoyal3 <goyaldivya631@gmail.com>
2026-04-16 13:29:08 +0530

    menu and category added

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,10 @@ import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
 import { connectDB } from "./config/db.js";
+import  connectCloudinary from "./config/cloudinary.js";
+import menuRoutes from "./routes/menuRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -11,12 +14,15 @@ const app = express();
 
 // database connection
 connectDB();
+connectCloudinary();
 
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/category", categoryRoutes);
+app.use("/api/menu", menuRoutes);
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
